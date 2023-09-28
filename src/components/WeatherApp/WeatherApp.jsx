491927e18f9bcd154ba81257c75ead20a2fafeff
Dhruvabhat24 <dhruvabhat24@gmail.com>
2023-09-28 17:33:01 +0530

    updated

--- src/components/WeatherApp/WeatherApp.jsx
+++ src/components/WeatherApp/WeatherApp.jsx
@@ -68,20 +68,20 @@ const WeatherApp = () => {
     <div className="weather-image">
       <img src={wicon} alt="cloud" />
     </div>
-    <div className="weather-temp">24 c</div>
-    <div className="weather-location">London</div>
+    <div className="weather-temp"></div>
+    <div className="weather-location"></div>
     <div class="data-container">
       <div className="element">
         <img src={humidity_icon} alt=""  className="icon"/>
         <div claqssName="data">
-          <div className="humidity-percent">64%</div>
+          <div className="humidity-percent"></div>
           <div className="text">Humidity</div>
         </div>
       </div>
       <div className="element">
         <img src={wind_icon} alt=""  className="icon"/>
         <div claqssName="data">
-          <div className="wind-rate">18km/h</div>
+          <div className="wind-rate"></div>
           <div className="text">Wind Speed</div>
         </div>
       </div>
